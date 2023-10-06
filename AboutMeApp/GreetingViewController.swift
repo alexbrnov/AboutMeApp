8924721c51f65e2e43947aa4ccbf4e1474df1d91
Alexandr Baranov <alexbrnov@gmail.com>
2023-10-07 00:34:15 +0300

    UI elements is set

--- AboutMeApp/GreetingViewController.swift
+++ AboutMeApp/GreetingViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class GreetingViewController: UIViewController {
-
+    // MARK: - IB Outlets
+    
+    // MARK: - Overrides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // MARK: - IB Actions
+    
+    
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
 }
--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+                                                          
     }
-    */
-
 }
