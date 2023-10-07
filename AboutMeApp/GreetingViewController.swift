4e6a59d3fa15b8a5b1bb8329c3963678b8721772
Alexandr Baranov <alexbrnov@gmail.com>
2023-10-07 18:46:01 +0300

    1.0.2

--- AboutMeApp/GreetingViewController.swift
+++ AboutMeApp/GreetingViewController.swift
@@ -9,25 +9,25 @@ import UIKit
 
 final class GreetingViewController: UIViewController {
     // MARK: - IB Outlets
-    
     @IBOutlet var greetingLabel: UILabel!
     
+    // MARK: - Public Properties
     var user: String!
+    
+    // MARK: - Private Properties
+    private let firstBgColor = UIColor(red: 0.514, green: 0.643, blue: 0.831, alpha: 1)
+    private let secondBgColor = UIColor(red: 0.714, green: 0.984, blue: 1, alpha: 1)
     
     // MARK: - Overrides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         greetingLabel.text = "Hello, \(user ?? "")!"
-
-       
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [firstBgColor.cgColor, secondBgColor.cgColor]
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
-    
-
-    // MARK: - IB Actions
-    
-    
-    
-    // MARK: - Public Methods
-    
-    // MARK: - Private Methods
 }
+
--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -8,21 +8,23 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
-    
+    // MARK: - IB Outlets
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
+    // MARK: - Private properties
     private let user = "user"
     private let pass = "pass"
     
+    // MARK: - Overrides methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-          super.touchesBegan(touches, with: event)
-          view.endEditing(true)
-      }
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         guard userNameTextField.text == user, passwordTextField.text == pass else {
             showAlert(
@@ -31,7 +33,6 @@ final class LoginViewController: UIViewController {
             )
             return false
         }
-        
         return true
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -39,12 +40,13 @@ final class LoginViewController: UIViewController {
         greetingVC.user = userNameTextField.text
     }
     
+    // MARK: - IB Actions
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         userNameTextField.text = ""
         passwordTextField.text = ""
     }
     
-    @IBAction func forgotUsernameButtonTapped() {
+    @IBAction func forgotUserNameButtonTapped() {
         showAlert(
             withTitle: "Oops",
             andMessage: "Try \(user)"
@@ -58,6 +60,7 @@ final class LoginViewController: UIViewController {
         )
     }
     
+    // MARK: - Private methods
     private func showAlert(withTitle title: String, andMessage message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
