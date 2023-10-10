67a7ce297dbd440c99add8748c3f4898d4e9e86e
Alexandr Baranov <alexbrnov@gmail.com>
2023-10-10 16:35:15 +0300

    1.0.3

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -17,10 +17,6 @@ final class LoginViewController: UIViewController {
     private let pass = "pass"
     
     // MARK: - Overrides methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
@@ -29,7 +25,8 @@ final class LoginViewController: UIViewController {
         guard userNameTextField.text == user, passwordTextField.text == pass else {
             showAlert(
                 withTitle: "Wrong login or pass",
-                andMessage: "Please enter correct values"
+                andMessage: "Please enter correct values",
+                textField: passwordTextField
             )
             return false
         }
@@ -37,37 +34,30 @@ final class LoginViewController: UIViewController {
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let greetingVC = segue.destination as? GreetingViewController else { return }
-        greetingVC.user = userNameTextField.text
+        greetingVC.user = user
     }
     
     // MARK: - IB Actions
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        userNameTextField.text = ""
         passwordTextField.text = ""
     }
     
-    @IBAction func forgotUserNameButtonTapped() {
-        showAlert(
-            withTitle: "Oops",
-            andMessage: "Try \(user)"
-        )
-    }
-    
-    @IBAction func forgotPassButtonTapped() {
-        showAlert(
-            withTitle: "Oops",
-            andMessage: "Try \(pass)"
-        )
+    @IBAction func helpButtonTapped(_ sender: UIButton) {
+        if sender.tag == 1 {
+            showAlert(withTitle: "Oops",andMessage: "Try \(user)")
+        } else {
+            showAlert(withTitle: "Oops", andMessage: "Try \(pass)")
+        }
     }
     
     // MARK: - Private methods
-    private func showAlert(withTitle title: String, andMessage message: String) {
+    private func showAlert(withTitle title: String, andMessage message: String, textField: UITextField? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            self.userNameTextField.text = ""
-            self.passwordTextField.text = ""
+            textField?.text = ""
         }
         alert.addAction(okAction)
         present(alert, animated: true)
     }
 }
+
